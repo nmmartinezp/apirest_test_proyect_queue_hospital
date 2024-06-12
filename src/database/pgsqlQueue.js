86dab44ac5cb18989db575f01383846756ade65e
nmmartinezp <nikimartin56@gmail.com>
2024-06-12 16:44:48 -0400

    update DB instance options on file psqlQueue

--- src/database/pgsqlQueue.js
+++ src/database/pgsqlQueue.js
@@ -4,7 +4,8 @@ const config  = require("../config");
 const sequelize = new Sequelize(config.pgsql_queues.database, config.pgsql_queues.user, config.pgsql_queues.password, {
     host: config.pgsql_queues.host,
     port: config.pgsql_queues.port,
-    dialect: 'postgres'
+    dialect: 'postgres',
+    timezone: '+00:00'
 });
 
 module.exports = sequelize;
