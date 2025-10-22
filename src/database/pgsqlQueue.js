e86843fff50d4229deb2f07141a948df7c8d539f
nmmartinezp <nikimartin56@gmail.com>
2025-10-22 12:35:47 -0400

    update for aws service

--- src/database/pgsqlQueue.js
+++ src/database/pgsqlQueue.js
@@ -1,11 +1,22 @@
-const { Sequelize } = require('sequelize');
-const config  = require("../config");
+const { Sequelize } = require("sequelize");
+const config = require("../config");
 
-const sequelize = new Sequelize(config.pgsql_queues.database, config.pgsql_queues.user, config.pgsql_queues.password, {
+const sequelize = new Sequelize(
+  config.pgsql_queues.database,
+  config.pgsql_queues.user,
+  config.pgsql_queues.password,
+  {
     host: config.pgsql_queues.host,
     port: config.pgsql_queues.port,
-    dialect: 'postgres',
-    timezone: '+00:00'
-});
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+    timezone: "+00:00",
+  }
+);
 
 module.exports = sequelize;
